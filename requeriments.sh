a5f10e5828ddc165391a511884bf42ad402f1c6e
Francisco Rodrigues <franciscosouzaacer@gmail.com>
2020-05-15 17:17:55 -0300

    Update requeriments.sh

--- requeriments.sh
+++ requeriments.sh
@@ -1,5 +1,7 @@
+#!/bin/bash
+
 echo "Install SpaCy.."
 pip install -U spacy
 
 echo "Install Model Lang En.."
-python -m spacy download en_core_web_sm
+python && import stanza && stanza.download('en')
