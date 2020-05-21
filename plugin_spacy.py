edf556a6c59f55495d21525fe816605c0cc91a63
Francisco Rodrigues <franciscosouzaacer@gmail.com>
2020-05-21 02:11:40 -0300

    Update plugin_spacy.py

--- plugin_spacy.py
+++ plugin_spacy.py
@@ -4,71 +4,79 @@
 import spacy, json
 
 from deepnlpf.core.iplugin import IPlugin
-from deepnlpf.core.boost import Boost
+
 
 class Plugin(IPlugin):
-
-    def __init__(self, id_pool, lang, document, pipeline):
-        self._id_pool = id_pool
+    def __init__(self, document, pipeline):
         self._document = document
-        self._pipeline = pipeline
+        self._processors = pipeline["tools"]["spacy"]["processors"]
 
-        self.nlp = spacy.load('en_core_web_sm')
+        self.nlp = spacy.load("en_core_web_sm")
 
     def run(self):
-        annotation = Boost().multithreading(
-            self.wrapper, self._document['sentences'])
+        annotation =self.wrapper()
         return self.out_format(annotation)
 
-    def wrapper(self, sentence):
-        doc = self.nlp(sentence) #generated document spacy.
+    def wrapper(self):
+        doc_formated = list()
 
-        data_tokens_list = []
+        for index, sentence in enumerate(self._document):
+            doc = self.nlp(sentence)
 
-        # Analisys in nivel token.
-        for idx, token in enumerate(doc):
-            data_token = {}
+            data_tokens_list = []
 
-            data_token['idx'] = idx
-            data_token['text'] = token.text
+            # Analisys in nivel token.
+            for idx, token in enumerate(doc):
+                data_token = {}
 
-            if "pos" in self._pipeline:
-                data_token['pos'] = token.pos_
-            if "tag" in self._pipeline:
-                data_token['tag'] = token.tag_
-            if "shape" in self._pipeline:
-                data_token['shape'] = token.shape_
-            if "is_alpha" in self._pipeline:
-                data_token['is_alpha'] = token.is_alpha
-            if "is_title" in self._pipeline:
-                data_token['is_title'] = token.is_title
-            if "like_num" in self._pipeline:
-                data_token['like_num'] = token.like_num
+                data_token["idx"] = idx
+                data_token["text"] = token.text
+
+                if "pos" in self._processors:
+                    data_token["pos"] = token.pos_
+                if "tag" in self._processors:
+                    data_token["tag"] = token.tag_
+                if "shape" in self._processors:
+                    data_token["shape"] = token.shape_
+                if "is_alpha" in self._processors:
+                    data_token["is_alpha"] = token.is_alpha
+                if "is_title" in self._processors:
+                    data_token["is_title"] = token.is_title
+                if "like_num" in self._processors:
+                    data_token["like_num"] = token.like_num
+
+                data_tokens_list.append(data_token)
+
+            list_chunks = list()
+            if "noun_chunks" in self._processors:
+                for chunk in doc.noun_chunks:
+                    data_chunk = {}
+                    data_chunk["text"] = chunk.text
+                    data_chunk["root_text"] = chunk.root.text
+                    data_chunk["root_dep_"] = chunk.root.dep_
+                    data_chunk["root_head_text"] = chunk.root.head.text
+                    json_data_chunk = data_chunk
+
+                    list_chunks.append(json_data_chunk)
+                    # print(chunk.text, chunk.root.text, chunk.root.dep_, chunk.root.head.text)
             
-            data_tokens_list.append(data_token)
-        
-        list_chunks = list()
-        if "noun_chunks" in self._pipeline:
-            for chunk in doc.noun_chunks:
-                data_chunk = {}
-                data_chunk['text'] = chunk.text
-                data_chunk['root_text'] = chunk.root.text
-                data_chunk['root_dep_'] = chunk.root.dep_
-                data_chunk['root_head_text'] = chunk.root.head.text
-                json_data_chunk = data_chunk
-                
-                list_chunks.append(json_data_chunk)
-                #print(chunk.text, chunk.root.text, chunk.root.dep_, chunk.root.head.text)
+            annotation_list = list()
 
-        data = {}
-        data['sentence'] = sentence
-        data['tokens'] = data_tokens_list
-        if "noun_chunks" in self._pipeline:
-            data['noun_chunks'] = list_chunks
+            data = {}
+            data["_id"] = index + 1
+            data["text"] = sentence
+            annotation_list.append({"tokens": data_tokens_list})
 
-        json_data_result = json.loads(json.dumps(data))
+            if "noun_chunks" in self._processors:
+                annotation_list.append({"noun_chunks": list_chunks})
+            
+            data["annotation"] = annotation_list
 
-        return json_data_result
+            json_data_result = json.loads(json.dumps(data))
+
+            doc_formated.append(json_data_result)
+
+        return doc_formated
 
     def out_format(self, doc):
-        pass
+        return doc
